5240f31bbd250fab1d5aa6f19bfb11b32eb8f4de
vivian-sam <56103899+vivian-sam@users.noreply.github.com>
2022-07-19 22:27:24 -0700

    added code for character sprite sheet and animation

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include "raylib.h"
+#include "raymath.h"
+
 
 
 int main()
@@ -17,6 +19,29 @@ int main()
     Vector2 backpos {0.0, 0.0}; //background x and y position relative to window
     float backrot{0.0}; //background rotation
     float backscale{4.0}; //background scale 
+    float speed{4.0};
+
+    //knight variables
+
+    Texture2D knight_idle = LoadTexture("characters/knight_idle_spritesheet.png");
+    Texture2D knight_run = LoadTexture("characters/knight_run_spritesheet.png");
+    Texture2D knight = LoadTexture("characters/knight_idle_spritesheet.png");
+    
+    Vector2 knightPos{
+        (float)windowWidth/2.0f - 4.0f * (0.5f * (float)knight.width/6),  //getting the rectangle of a single sprite then scale it by 4 because it's tiny
+        (float)windowHeight/2.0f - 4.0f * (0.5f * (float)knight.height)
+    };
+    // 1 : facing right, -1 : facing left
+    float rightLeft{1.f};
+
+    //animation varialbes
+    float runningTime{};
+    int knightFrame{0};
+    const int maxKnightFrames{6};
+    const float updateTime{1.f/12.f};
+
+    //origin is the anchor where you rotate or scale a vector
+    Vector2 origin {0.0, 0.0};
 
     while (!WindowShouldClose())
     {
@@ -24,14 +49,52 @@ int main()
         BeginDrawing();
         ClearBackground(WHITE);
 
+        Vector2 direction{};                        //vector to save direction character wants to go
+
+        if (IsKeyDown(KEY_A)) direction.x -= 1.0;   //move left
+        if (IsKeyDown(KEY_D)) direction.x += 1.0;   //move right
+        if (IsKeyDown(KEY_W)) direction.y -= 1.0;   //move up
+        if (IsKeyDown(KEY_S)) direction.y += 1.0;   // move down
+        if (Vector2Length(direction) != 0.0) //check if direction vector is not 0
+        {
+            // set backpos = backpos - direction
+            // scales up the normalized vector to match speed
+            backpos = Vector2Subtract(backpos, Vector2Scale(Vector2Normalize(direction), speed) );
+            
+            // check if facing left
+            direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f;
+            knight = knight_run;
+            
+        }
+        else knight = knight_idle;
+
         //draw the background to the game, takes input of the spritesheet, position, rotation and scale
         DrawTextureEx(background, backpos, backrot,  backscale, WHITE);
+        
+        //update animation frame
+        runningTime += GetFrameTime();
+
+        if (runningTime >= updateTime)
+        {
+            ++knightFrame;
+            runningTime = 0.f;
+            if (knightFrame > maxKnightFrames) knightFrame = 0;
+        }
+
+
+        //Draw knight character
+        //rectangle for where to draw knight on screen
+        Rectangle knightDest {knightPos.x, knightPos.y, 4.0f * (float)knight.width/6.0f, 4.0f * (float)knight.height};
+        //rectangle for which knight sprite frame we want
+        Rectangle knightSource {(float)knightFrame*knight.width/6.f, 0.f, rightLeft * (float)knight.width/6.f, (float)knight.height};
+        DrawTexturePro(knight, knightSource, knightDest, origin, 0, WHITE);
         
         //stop drawing (graphics)
         EndDrawing();
     }
 
     UnloadTexture(background);
+    UnloadTexture(knight);
 
     CloseWindow();
 
